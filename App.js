fc4871989100c6809dc1377db2bc13c26ad842e3
R <ramz9_97@hotmail.com>
2022-02-07 20:09:59 +0200

    added login form structure and main context

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import StackScreen from './navigators/DrawerNav';
+import DrawerNavigator from './navigators/DrawerNav';
+import {StatusBar} from 'react-native';
+import {MainProvider} from './contexts/MainContext';
 
 const App = () => {
-  return <StackScreen />;
+  return (
+    <>
+      <MainProvider>
+        <DrawerNavigator />
+        <StatusBar style="auto" />
+      </MainProvider>
+    </>
+  );
 };
 
 export default App;
