3eb97c246acbe7a2caacc9c1f0d9bfc49dcbc580
myxmxm <myxmxm@gmail.com>
2022-02-13 22:49:02 +0200

    add profile image(hard code)

--- views/Profile.js
+++ views/Profile.js
@@ -1,5 +1,5 @@
 import React, {useContext} from 'react';
-import {Text, View, Button} from 'react-native';
+import {Text, View, Button, Image} from 'react-native';
 import {PropTypes} from 'prop-types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {MainContext} from '../contexts/MainContext';
@@ -9,10 +9,16 @@ const Profile = ({navigation}) => {
   console.log('user:', user);
   return (
     <View>
+      <Image
+        source={{uri: 'http://placekitten.com/640'}}
+        style={{width: 100, height: 100, borderRadius: 400 / 2}}
+        resizeMode="contain"
+      />
       <Text>username: {user.username}</Text>
       <Text>full name: {user.full_name}</Text>
       <Text>user email: {user.email}</Text>
       <Text>user id: {user.user_id}</Text>
+
       <Button
         title="Log out"
         onPress={async () => {
