f394b1e07fb3be675d98e5318df0044ab83b9018
myxmxm <myxmxm@gmail.com>
2022-02-13 22:25:54 +0200

    display user info in profile page

--- contexts/MainContext.js
+++ contexts/MainContext.js
@@ -5,9 +5,13 @@ const MainContext = React.createContext({});
 
 const MainProvider = (props) => {
   const [loggedIn, setLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+  const [update, setUpdate] = useState(0);
 
   return (
-    <MainContext.Provider value={{loggedIn, setLoggedIn}}>
+    <MainContext.Provider
+      value={{loggedIn, setLoggedIn, user, setUser, update, setUpdate}}
+    >
       {props.children}
     </MainContext.Provider>
   );
--- views/Profile.js
+++ views/Profile.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useContext} from 'react';
+import {Text, View, Button} from 'react-native';
 import {PropTypes} from 'prop-types';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {MainContext} from '../contexts/MainContext';
 
 const Profile = ({navigation}) => {
+  const {setLoggedIn, user} = useContext(MainContext);
+  console.log('user:', user);
   return (
     <View>
-      <Text>Profile Screen</Text>
+      <Text>username: {user.username}</Text>
+      <Text>full name: {user.full_name}</Text>
+      <Text>user email: {user.email}</Text>
+      <Text>user id: {user.user_id}</Text>
+      <Button
+        title="Log out"
+        onPress={async () => {
+          await AsyncStorage.clear();
+          setLoggedIn(false);
+        }}
+      />
     </View>
   );
 };
