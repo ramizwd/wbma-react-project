f009ecb0c36597ed78ca2dc5e5d847a9343fc131
myxmxm <myxmxm@gmail.com>
2022-02-17 22:55:07 +0200

    updata

--- views/Search.js
+++ views/Search.js
@@ -1,14 +1,18 @@
 import React, {useContext, useEffect, useState} from 'react';
 import {View, Alert, ScrollView, StyleSheet} from 'react-native';
 import {PropTypes} from 'prop-types';
-import {Button, Input, Card} from '@ui-kitten/components';
+import {Button, Input} from '@ui-kitten/components';
 import {MainContext} from '../contexts/MainContext';
 import {useForm, Controller} from 'react-hook-form';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useMedia} from '../hooks/ApiHooks';
+import Card from '../components/Card';
+import CardContent from '../components/CardContent';
+import {List} from '@ui-kitten/components';
 
 const Search = ({navigation}) => {
   const {searchMedia} = useMedia();
+  const [searchResult, setSearchResult] = useState([]);
   const {
     control,
     handleSubmit,
@@ -19,6 +23,8 @@ const Search = ({navigation}) => {
     },
     mode: 'onBlur',
   });
+  const {mediaArray} = useMedia();
+  let result;
 
   const onSubmit = async (data) => {
     const token = await AsyncStorage.getItem('token');
@@ -26,37 +32,51 @@ const Search = ({navigation}) => {
 
     try {
       const response = await searchMedia(data, token);
-      console.log('(Search) search result:', response);
+      setSearchResult(response);
+      // console.log('(Search) search result:', result);
+      console.log('mediaarry', mediaArray);
     } catch (error) {
-      console.error(error.message);
+      // console.error(error.message);
       console.log('search error');
     }
   };
 
-  return (
-    <ScrollView>
-      <Card>
-        <Controller
-          control={control}
-          rules={{
-            required: {value: true, message: 'This is required.'},
-          }}
-          render={({field: {onChange, onBlur, value}}) => (
-            <Input
-              onBlur={onBlur}
-              onChangeText={onChange}
-              value={value}
-              autoCapitalize="none"
-              placeholder="Search by title"
-              errorMessage={errors.title && errors.title.message}
-            />
-          )}
-          name="title"
-        />
+  useEffect(() => {
+    onSubmit();
+  }, result);
 
-        <Button title="Search" onPress={handleSubmit(onSubmit)} />
-      </Card>
-    </ScrollView>
+  return (
+    <View>
+      <Controller
+        control={control}
+        rules={{
+          required: {value: true, message: 'This is required.'},
+        }}
+        render={({field: {onChange, onBlur, value}}) => (
+          <Input
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            autoCapitalize="none"
+            placeholder="Search by title"
+            errorMessage={errors.title && errors.title.message}
+          />
+        )}
+        name="title"
+      />
+
+      <Button title="Search" onPress={handleSubmit(onSubmit)}>
+        Search
+      </Button>
+
+      <List
+        data={searchResult}
+        keyExtractor={(item) => item.file_id.toString()}
+        renderItem={({item}) => (
+          <CardContent post={item} navigation={navigation} />
+        )}
+      ></List>
+    </View>
   );
 };
 
