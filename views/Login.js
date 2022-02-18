4c544a5a42947530c214e901933d0bd35cf6bb15
R <ramz9_97@hotmail.com>
2022-02-18 18:03:51 +0200

    Login UI done

--- views/Login.js
+++ views/Login.js
@@ -1,10 +1,11 @@
 import React, {useContext, useEffect} from 'react';
-import {Button, View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import LoginForm from '../components/LoginForm';
 import PropTypes from 'prop-types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useUser} from '../hooks/ApiHooks';
 import {MainContext} from '../contexts/MainContext';
+import {Button, Layout, Text} from '@ui-kitten/components';
 
 const Login = ({navigation}) => {
   const {getUserByToken} = useUser();
@@ -30,12 +31,41 @@ const Login = ({navigation}) => {
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text>Moment</Text>
       <LoginForm />
-      <Button title="Sing up" onPress={() => navigation.navigate('Register')} />
+      <Layout style={styles.singUp}>
+        <Text style={styles.singUpText}>Don't have an account?</Text>
+        <Button
+          style={styles.button}
+          onPress={() => navigation.navigate('Register')}
+          appearance="ghost"
+          status="success"
+        >
+          Sing Up
+        </Button>
+      </Layout>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '80%',
+  },
+  singUp: {
+    marginTop: 20,
+    flexDirection: 'row',
+    backgroundColor: '#F2F2F2',
+  },
+  singUpText: {
+    textAlign: 'center',
+    justifyContent: 'center',
+    marginTop: 13,
+  },
+});
 
 Login.propTypes = {
   navigation: PropTypes.object,
