e5394a388d9d22b2b008e7c639d76730d5c6211c
R <ramz9_97@hotmail.com>
2022-02-28 18:16:11 +0200

    fixed register view scrolling

--- views/Register.js
+++ views/Register.js
@@ -1,53 +1,65 @@
 import React from 'react';
 import {Keyboard, StyleSheet, TouchableOpacity} from 'react-native';
-import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 import RegisterForm from '../components/RegisterForm';
 import {Button, Layout, Text} from '@ui-kitten/components';
 import PropTypes from 'prop-types';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 // Register views
 const Register = ({navigation}) => {
   return (
-    <KeyboardAwareScrollView>
-      <TouchableOpacity
-        onPress={() => Keyboard.dismiss()}
-        style={{flex: 1}}
-        activeOpacity={1}
-      >
-        <Layout style={styles.container}>
-          <Text>Moment</Text>
-          <RegisterForm />
+    <Layout
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+      }}
+    >
+      <KeyboardAwareScrollView style={{width: '100%'}}>
+        <TouchableOpacity
+          onPress={() => Keyboard.dismiss()}
+          style={{flex: 1}}
+          activeOpacity={1}
+        >
+          <Layout>
+            <Text style={styles.header}>Moment</Text>
+            <RegisterForm />
 
-          <Layout style={styles.singIn}>
-            <Text style={styles.singInText}>Already have an account?</Text>
-            <Button
-              style={styles.singInBtn}
-              onPress={() => navigation.navigate('Login')}
-              appearance="ghost"
-              status="success"
-            >
-              {(evaProps) => (
-                <Text {...evaProps} style={styles.singInBtn}>
-                  Sign In
-                </Text>
-              )}
-            </Button>
+            <Layout style={styles.singIn}>
+              <Text style={styles.singInText}>Already have an account?</Text>
+              <Button
+                style={styles.singInBtn}
+                onPress={() => navigation.navigate('Login')}
+                appearance="ghost"
+                status="success"
+              >
+                {(evaProps) => (
+                  <Text {...evaProps} style={styles.singInBtn}>
+                    Sign In
+                  </Text>
+                )}
+              </Button>
+            </Layout>
           </Layout>
-        </Layout>
-      </TouchableOpacity>
-    </KeyboardAwareScrollView>
+        </TouchableOpacity>
+      </KeyboardAwareScrollView>
+    </Layout>
   );
 };
 
 const styles = StyleSheet.create({
+  header: {
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
   singIn: {
-    marginTop: 20,
     flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 20,
   },
   singInText: {
     textAlign: 'center',
