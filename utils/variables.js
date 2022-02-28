8f5e6fcae6ae1e8168403bdf67532116f4fa8d5a
R <63073421+ramizwd@users.noreply.github.com>
2022-02-28 13:48:33 +0200

    using env for appId

--- utils/variables.js
+++ utils/variables.js
@@ -1,6 +1,5 @@
 const uploadsUrl = 'https://media.mw.metropolia.fi/wbma/uploads/';
 const baseUrl = 'https://media.mw.metropolia.fi/wbma/';
-const appId = 'e87y2387y44';
 const tagDivider = 'uuu';
 
-export {baseUrl, appId, uploadsUrl, tagDivider};
+export {baseUrl, uploadsUrl, tagDivider};
