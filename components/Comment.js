6e2182215d5df110d4cd5baaeeeac6cca110e7c5
Tatu <tatu.d.ihaksi@gmail.com>
2022-03-01 06:37:20 +0200

    delete comment ready and profile navigation from comment

--- components/Comment.js
+++ components/Comment.js
@@ -1,19 +1,22 @@
-import {View, Text} from 'react-native';
+import {View, Text, Alert, StyleSheet} from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {PropTypes} from 'prop-types';
-import {useUser} from '../hooks/ApiHooks';
+import {useComment, useUser} from '../hooks/ApiHooks';
 import Avatar from './Avatar';
-import {Button} from '@ui-kitten/components';
+import {Button, Icon, Layout} from '@ui-kitten/components';
 import {MainContext} from '../contexts/MainContext';
 import moment from 'moment';
+import {TouchableWithoutFeedback} from '@ui-kitten/components/devsupport';
 
 // Component for individual comment. Takes comment prop.
 // Renders comments owners username and avatar, comment text and time when comment was added
-const Comment = ({comment}) => {
+const Comment = ({comment, navigation}) => {
   const [commentOwner, setCommentOwner] = useState([]);
   const {getUserById} = useUser();
+  const {deleteComment} = useComment();
   const {user} = useContext(MainContext);
+  const {setUpdate, update} = useContext(MainContext);
 
   // Fetch user who posted the comment
   const getCommentOwner = async () => {
@@ -26,53 +29,88 @@ const Comment = ({comment}) => {
     }
   };
 
-  // Delete comment (didn't get to work yet)
-  /* const deleteComment = async () => {
-    console.log('delete pressed', comment.comment_id);
-    try {
-      setUpdate(update + 1);
-      const token = await AsyncStorage.getItem('token');
-      await deleteComment(comment.comment_id, token);
-      console.log('comment deleted');
-    } catch (error) {
-      console.error('deleteComment error', error);
-    }
-  }; */
+  // Delete comment
+  const removeComment = () => {
+    Alert.alert('Delete', 'This comment will be deleted!', [
+      {text: 'Cancel'},
+      {
+        text: 'OK',
+        onPress: async () => {
+          try {
+            const token = await AsyncStorage.getItem('token');
+            const response = await deleteComment(comment.comment_id, token);
+            if (response) {
+              setUpdate(update + 1);
+              Alert.alert('Deleted', 'The comment has been deleted');
+            }
+          } catch (error) {
+            console.error('deleteComment error', error);
+          }
+        },
+      },
+    ]);
+  };
+
+  const renderDeleteIcon = () => (
+    <Icon name="trash" style={{width: 25, height: 25}} />
+  );
 
   // Fetch comments owner
   useEffect(() => {
     getCommentOwner();
-  }, []);
+  }, [update]);
 
   return (
     <View style={{borderWidth: 1}}>
-      <View style={{flexDirection: 'row'}}>
-        <Text>{commentOwner.username}</Text>
+      <Layout
+        style={{
+          flexDirection: 'row',
+        }}
+      >
+        <TouchableWithoutFeedback
+          style={{flexDirection: 'row'}}
+          onPress={() => navigation.navigate('User profile', {file: comment})}
+        >
+          <Avatar userAvatar={comment.user_id} />
+          <Text style={{marginLeft: 5}}>{commentOwner.username}</Text>
+        </TouchableWithoutFeedback>
 
-        <Avatar userAvatar={comment.user_id} />
-      </View>
+        {comment.user_id === user.user_id && (
+          <Button
+            style={{
+              height: 30,
+              width: 30,
+              justifyContent: 'flex-start',
+              alignItems: 'flex-start',
+              textAlign: 'right',
+            }}
+            onPress={() => {
+              removeComment();
+            }}
+            appearance="ghost"
+            accessoryRight={renderDeleteIcon}
+          ></Button>
+        )}
+      </Layout>
       <View>
         <Text>{comment.comment}</Text>
         <Text style={{textAlign: 'right', marginRight: 5}}>{`Posted: ${moment(
           comment.time_added
         ).fromNow()}`}</Text>
-
-        {comment.user_id === user.user_id && (
-          <Button
-            onPress={() => {
-              /* deleteComment(); */
-            }}
-          >
-            Delete
-          </Button>
-        )}
       </View>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+});
+
 Comment.propTypes = {
   comment: PropTypes.object,
+  navigation: PropTypes.object,
 };
 
 export default Comment;
